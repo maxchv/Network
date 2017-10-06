0a122d63a054f42eb447f07a9b121544246cc020
Максим Шаптала <shaptala@itstep1.dp.ua>
2017-10-06 11:58:47 +0300

    added questions

--- Papercut-5.0.9/src/Papercut.Core/Domain/Message/IReceivedDataHandler.cs
+++
@@ -1,9 +0,0 @@
-namespace Papercut.Core.Domain.Message
-{
-    using System.Text;
-
-    public interface IReceivedDataHandler
-    {
-        void HandleReceived(string message, string[] recipients, Encoding connectionEncoding);
-    }
-}
